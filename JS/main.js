97b6b1c17b3c1a11ebe19c170d4e9365958df1c0
Dr. Cranium <edlomail@wi.rr.com>
2026-05-25 17:43:10 -0500

    Fixed mouse logic on "Delete All Lists" button

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,8 @@ let btn = document.getElementById("button");
 let letters = document.getElementById("Header1");
 let body = document.getElementsByTagName("body")[0];
 
+let delAllListsBtn = document.getElementById("deleteAllLists");
+
 document.addEventListener("DOMContentLoaded", ()=>{
   ////
   
@@ -14,7 +16,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
   document.addEventListener("mousedown", event => {
     //window.electronAPI.logToTerminal("Button Was Clicked!");
 
-    clickAddListBtn(event);
+    checkMouseDown(event);
 
     /*if(body.style.backgroundColor == "white" || body.style.backgroundColor == ""){
       body.style.backgroundColor = "#222";
@@ -26,48 +28,76 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }*/
   });
 
-  btn.addEventListener("mouseup", event =>{
+  document.addEventListener("mouseup", event =>{
 
-    mouseUpAddListBtn(event);
+    checkMouseUp(event);
 
   });
 
-  btn.addEventListener("mouseover", event =>{
-    mouseOverAddListBtn(event);
+  document.addEventListener("mouseover", event =>{
+    checkMouseHover(event);
   });
 
-  btn.addEventListener("mouseleave", event =>{
-    mouseLeaveAddListBtn(event);
+  document.addEventListener("mouseout", event =>{
+    checkMouseLeave(event);
+    
   });
 
   ///
 });
 
-function clickAddListBtn(event){
+function checkMouseDown(event){
 
   if(event.target == btn){
     btn.style.backgroundColor = "blue";
     btn.style.borderColor = "black";
     btn.style.borderWidth = "4px";      
   }
+
+  if(event.target == delAllListsBtn){
+    delAllListsBtn.style.backgroundColor = "rgb(183, 65, 14)"; //rust color
+    delAllListsBtn.style.borderColor = "black";
+    delAllListsBtn.style.borderWidth = "4px";      
+
+    console.log("Orange down!");
+  }
 }
 
-function mouseUpAddListBtn(event){
+function checkMouseUp(event){
 
   if(event.target == btn){
     btn.style.backgroundColor = "aqua";
     btn.style.borderWidth = "2px";
   }
+
+  if(event.target == delAllListsBtn){
+    delAllListsBtn.style.backgroundColor = "rgb(207, 130, 13)"; // A shade of orange
+    delAllListsBtn.style.borderWidth = "2px";
+
+    console.log("Orange up!");
+  }
 }
 
-function mouseOverAddListBtn(event){
+function checkMouseHover(event){
   if(event.target == btn){
     btn.style.backgroundColor = "gray";
   }
-}
 
-function mouseLeaveAddListBtn(event){
-  if(event.target == btn){
-    btn.style.backgroundColor = null;
+  if(event.target == delAllListsBtn){
+    delAllListsBtn.style.backgroundColor = "gray";
+    console.log("Orange Hover!");
   }
 }
+
+function checkMouseLeave(event){
+  if(event.target == btn){
+    btn.style.backgroundColor = "aqua";
+  }
+
+  if(event.target == delAllListsBtn){
+    delAllListsBtn.style.backgroundColor = "rgb(207, 130, 13)";
+    console.log("Orange Leave!");
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
